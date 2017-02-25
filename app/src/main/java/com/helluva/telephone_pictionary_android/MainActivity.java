94c56812f7bc01f7c7fb9f5fbc0623a1890ec297
thompsonate <nate@natethompson.io>
2017-02-25 01:31:06 -0500

    sketch and text classes and XMLs

--- app/src/main/java/com/helluva/telephone_pictionary_android/MainActivity.java
+++ app/src/main/java/com/helluva/telephone_pictionary_android/MainActivity.java
@@ -65,20 +65,26 @@ public class MainActivity extends AppCompatActivity {
 
                 alertBuilder.show();
 
+<<<<<<< Updated upstream
             }
 
         });
     }
+=======
+}
+});
+        }
+>>>>>>> Stashed changes
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -86,10 +92,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+        return true;
         }
 
         return super.onOptionsItemSelected(item);
+<<<<<<< Updated upstream
     }
 
+=======
+        }
+>>>>>>> Stashed changes
 }

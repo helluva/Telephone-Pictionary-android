8e47db70e76fd5bf544f53f4238b9d1a0c8f3707
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-02-25 01:21:36 -0500

    players

---
+++ app/src/main/java/com/helluva/telephone_pictionary_android/HostPlayer.java
@@ -0,0 +1,17 @@
+package com.helluva.telephone_pictionary_android;
+
+/**
+ * Created by Gabor Siffel on 2/25/2017.
+ */
+
+public class HostPlayer extends Player {
+
+    public HostPlayer() {
+        super();
+    }
+
+    public HostPlayer(String username) {
+        super(username);
+    }
+
+}

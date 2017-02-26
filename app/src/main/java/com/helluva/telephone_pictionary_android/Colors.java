65694e57404f09ffa3d3081c0c06c06535b45ee8
JakeWaldner <JakeWaldner@users.noreply.github.com>
2017-02-25 17:25:46 -0900

    color picker

--- app/src/main/java/com/helluva/telephone_pictionary_android/Colors.java
+++ app/src/main/java/com/helluva/telephone_pictionary_android/Colors.java
@@ -3,19 +3,25 @@ package com.helluva.telephone_pictionary_android;
 import android.graphics.Color;
 
 public enum Colors {
-    BLACK("Black"),
-    RED("Red"),
-    GREEN("Green"),
-    BLUE("Blue");
+    BLACK("Black", Color.BLACK),
+    RED("Red", Color.RED),
+    GREEN("Green", Color.GREEN),
+    BLUE("Blue", Color.BLUE);
 
     //may need to make public????
-    private final String selectedColor;
+    private final String selectedColorName;
+    private final int selectedColor;
 
-    Colors(String selectedColor) {
+    Colors(String selectedColorName,int selectedColor) {
+        this.selectedColorName = selectedColorName;
         this.selectedColor = selectedColor;
     }
 
     public String toString() {
+        return selectedColorName;
+    }
+
+    public int getColor() {
         return selectedColor;
     }
 
